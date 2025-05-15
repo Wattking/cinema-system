92a4436351b32e61be0729ddda07547638565cea
LEGION-Y7000-20\FCZ <2677464694@qq.com>
2025-05-15 10:08:33 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  define: {
-    __VUE_PROD_DEVTOOLS__: true, // 强制启用 DevTools
+  base: '/', // 生产环境基础路径
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://m.maizuo.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
