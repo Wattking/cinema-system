b5b13128177b9523e7ec0ed7f04552cc01dd1be4
Wattking <163096637+Wattking@users.noreply.github.com>
2025-05-15 19:13:42 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/cinema-system/',  // 必须与你的仓库名一致
-  build: {
-    outDir: 'dist'
-  }
+  base: '/cinema-system/'  // 必须与你的仓库名一致
 })
